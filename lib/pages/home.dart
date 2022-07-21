a8a44c7e86dd430b879f91d5f93091ab8d05a707
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2022-07-21 17:42:46 +0600

    Add resource icons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,8 +10,73 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Islami Jindegi'),
       ),
-      body: const Center(
-        child: Text('Home Page Contents'),
+      body: GridView.count(
+        crossAxisCount: 3,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/quran.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/book.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/bayan.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/malfuzat.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/masail.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/dua.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/article.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/news.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/madrasah.svg',
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SvgPicture.asset(
+              'assets/images/icons/namaz-time.svg',
+            ),
+          ),
+        ],
       ),
       drawer: const Drawer(),
     );
