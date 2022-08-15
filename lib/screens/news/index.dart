588d48dfbd52ebc5ecbd96e24bdfe8b90a477883
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2022-08-15 19:36:08 +0600

    Add list of news

--- lib/screens/news/index.dart
+++ lib/screens/news/index.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:native_app/widgets/layouts/scaffold.dart';
+import 'package:native_app/styles/settings/theme_colors.dart';
 
 class News extends StatelessWidget {
   const News({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MyScaffold(
-      title: Text('News'),
-      body: Center(
-        child: Text(
-          'List of News',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
-        ),
+    List<String> items = List<String>.generate(50, (i) => 'News Item $i');
+
+    return MyScaffold(
+      title: const Text('News'),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: ThemeColors().themeColor7,
+            ),
+            padding: const EdgeInsets.all(15),
+            margin: const EdgeInsets.only(bottom: 15),
+            child: Text(
+              items[index],
+              style: TextStyle(color: ThemeColors().themeColor4),
+            ),
+          );
+        },
       ),
     );
   }
