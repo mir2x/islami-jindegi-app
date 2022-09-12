a0a4a6baa15156dea4b16571fabae7de3a7fadd3
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2022-09-12 19:04:28 +0600

    Refactor formatDate

--- lib/helpers/format-date.dart
+++ lib/helpers/format-date.dart
@@ -1,10 +1,6 @@
 import 'package:intl/intl.dart';
 
 String formatDate(date) {
-  var inputFormat = DateFormat('yyyy-MM-ddTHH:mm:ss');
-  var inputDate = inputFormat.parse(date); // <-- dd/MM 24H format
-
-  var outputFormat = DateFormat('dd MMMM yyyy');
-  var outputDate = outputFormat.format(inputDate);
-  return outputDate;
+  var inputDate = DateFormat('yyyy-MM-ddTHH:mm:ss').parse(date);
+  return DateFormat('dd MMMM yyyy').format(inputDate);
 }
