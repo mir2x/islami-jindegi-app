1eab95f90ca5d4b0167c7e27fba47ab065b8fcc9
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2022-10-24 19:04:44 +0600

    Update Book chapter list display

--- lib/screens/books/book.dart
+++ lib/screens/books/book.dart
@@ -9,6 +9,7 @@ import 'package:native_app/objects/all_models_query.dart';
 import 'package:native_app/screens/error_pages/model_exception_handler.dart';
 import 'package:native_app/widgets/layouts/scaffold.dart';
 import 'package:native_app/widgets/utils/full_screen_loader.dart';
+import 'package:native_app/theme/colors.dart';
 
 class Book extends ConsumerWidget {
   const Book({super.key});
@@ -30,75 +31,99 @@ class Book extends ConsumerWidget {
       data: (book) {
         return MyScaffold(
           title: Text(book.title),
-          body: Container(
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
-            child: InfiniteList(
-              resourceFetcher: (int pageKey, int pageSize) async {
-                AllModelsQuery query = AllModelsQuery(
-                  repository: 'chapters',
-                  params: {
-                    'page': pageKey,
-                    'per_page': pageSize,
-                    'book_id': book.id,
-                    'include': 'subchapters',
-                  },
-                );
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 30,
+                  horizontal: 15,
+                ),
+                child: Text('Contents', style: textTheme.labelLarge),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    left: 15,
+                    right: 15,
+                    bottom: 30,
+                  ),
+                  child: InfiniteList(
+                    resourceFetcher: (int pageKey, int pageSize) async {
+                      AllModelsQuery query = AllModelsQuery(
+                        repository: 'chapters',
+                        params: {
+                          'page': pageKey,
+                          'per_page': pageSize,
+                          'book_id': book.id,
+                          'include': 'subchapters',
+                        },
+                      );
 
-                return await ref.read(allModelsProvider(query).future);
-              },
-              itemBuilder: (_, chapter, __) {
-                if (chapter.subchapters.length > 0) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        chapter.title,
-                        style: textTheme.titleMedium,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(
-                          left: 30,
-                          top: 15,
-                          bottom: 15,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ...chapter.subchapters.map((subchapter) {
-                              return InkWell(
-                                onTap: () => QR.to(
-                                  'books/${book.id}/subchapters/${subchapter.id}',
+                      return await ref.read(allModelsProvider(query).future);
+                    },
+                    itemBuilder: (_, chapter, __) {
+                      if (chapter.subchapters.length > 0) {
+                        return Container(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                chapter.title,
+                                style: textTheme.titleLarge?.copyWith(
+                                  color: ThemeColors.color8,
                                 ),
-                                child: Container(
-                                  padding: const EdgeInsets.only(bottom: 15),
-                                  child: Text(
-                                    subchapter.title,
-                                    style: textTheme.titleMedium,
-                                  ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.only(
+                                  left: 30,
+                                  top: 15,
                                 ),
-                              );
-                            }),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                } else {
-                  return InkWell(
-                    onTap: () => QR.to(
-                      'books/${book.id}/chapters/${chapter.id}',
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.only(bottom: 15),
-                      child: Text(
-                        chapter.title,
-                        style: textTheme.titleMedium,
-                      ),
-                    ),
-                  );
-                }
-              },
-            ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ...chapter.subchapters.map((subchapter) {
+                                      return InkWell(
+                                        onTap: () => QR.to(
+                                          'books/${book.id}/subchapters/${subchapter.id}',
+                                        ),
+                                        child: Container(
+                                          padding: const EdgeInsets.only(
+                                            bottom: 15,
+                                          ),
+                                          child: Text(
+                                            subchapter.title,
+                                            style: textTheme.titleMedium,
+                                          ),
+                                        ),
+                                      );
+                                    }),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        return InkWell(
+                          onTap: () => QR.to(
+                            'books/${book.id}/chapters/${chapter.id}',
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: Text(
+                              chapter.title,
+                              style: textTheme.titleLarge,
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
