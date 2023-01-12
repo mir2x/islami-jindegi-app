05487b47f9a996796b6eaa52804c07c6b29264e5
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-01-12 19:27:22 +0600

    Add autoDispose to model providers

--- lib/providers/all_models.dart
+++ lib/providers/all_models.dart
@@ -2,8 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:native_app/objects/all_models_query.dart';
 import 'package:native_app/main.data.dart';
 
-final allModelsProvider = FutureProvider.family<List, AllModelsQuery>(
-    (ref, AllModelsQuery query) async {
+final allModelsProvider = FutureProvider.autoDispose
+    .family<List, AllModelsQuery>((ref, AllModelsQuery query) async {
   await ref.watch(repositoryInitializerProvider.future);
 
   var resources = await query.repository.findAll(
--- lib/providers/single_model.dart
+++ lib/providers/single_model.dart
@@ -2,8 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:native_app/objects/single_model_query.dart';
 import 'package:native_app/main.data.dart';
 
-final singleModelProvider = FutureProvider.family<dynamic, SingleModelQuery>(
-    (ref, SingleModelQuery query) async {
+final singleModelProvider = FutureProvider.autoDispose
+    .family<dynamic, SingleModelQuery>((ref, SingleModelQuery query) async {
   await ref.watch(repositoryInitializerProvider.future);
 
   var resource = await query.repository.findOne(
