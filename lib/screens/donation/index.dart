ea895492194c93e61a71c6e03630f24c41046eaa
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-01-17 14:56:36 +0600

    Update donation page display

--- lib/screens/donation/index.dart
+++ lib/screens/donation/index.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:native_app/main.data.dart';
 import 'package:native_app/widgets/layouts/scaffold.dart';
+import 'package:native_app/providers/all_models.dart';
+import 'package:native_app/objects/all_models_query.dart';
+import 'package:native_app/widgets/presentation/item_content.dart';
+import 'package:native_app/widgets/utils/html_text.dart';
 
-class Donation extends StatelessWidget {
+class Donation extends ConsumerWidget {
   const Donation({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var textTheme = Theme.of(context).textTheme;
+
+    var query = AllModelsQuery(
+      repository: ref.pages,
+      params: const {'slug': 'donation', 'quantity': 1},
+    );
+
+    var modelQuery = ref.watch(allModelsProvider(query));
 
     return MyScaffold(
       title: const Text('Donation'),
-      body: Center(
-        child: Text(
-          'Info about Donation',
-          style: textTheme.labelMedium,
-        ),
+      body: modelQuery.when(
+        loading: () {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        error: (error, _) => Text(error.toString()),
+        data: (resources) {
+          var item = resources[0];
+
+          return ItemContent(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                child: Text(
+                  item.title,
+                  style: textTheme.headlineMedium,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 30),
+                child: HtmlText(
+                  text: item.body,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
