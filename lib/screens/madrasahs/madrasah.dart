d37223f41f437187b3d32fafdfe7415b2f1a1fd1
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-01-25 17:55:05 +0600

    Add sections to Madrasah display

--- lib/screens/madrasahs/madrasah.dart
+++ lib/screens/madrasahs/madrasah.dart
@@ -8,18 +8,21 @@ import 'package:native_app/screens/error_pages/model_exception_handler.dart';
 import 'package:native_app/widgets/layouts/scaffold.dart';
 import 'package:native_app/widgets/utils/full_screen_loader.dart';
 import 'package:native_app/widgets/presentation/item_content.dart';
+import 'package:native_app/widgets/presentation/description_item.dart';
 import 'package:native_app/widgets/utils/html_text.dart';
+import 'package:native_app/widgets/responsive/image.dart';
+import 'package:native_app/theme/colors.dart';
 
 class Madrasah extends ConsumerWidget {
   const Madrasah({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var textTheme = Theme.of(context).textTheme;
-
     var query = SingleModelQuery(
       repository: ref.madrasahs,
       id: QR.params['id'].toString(),
+      params: const {'include': 'madrasah-infos,madrasah-photos'},
+      remote: true,
     );
 
     var modelQuery = ref.watch(singleModelProvider(query));
@@ -30,25 +33,188 @@ class Madrasah extends ConsumerWidget {
       data: (resource) {
         return MyScaffold(
           title: Text(resource.title),
-          body: ItemContent(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 15),
-                child: Text(
-                  resource.title,
-                  style: textTheme.headlineMedium,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 30),
-                child: HtmlText(
-                  text: resource.introduction,
-                ),
-              ),
-            ],
-          ),
+          body: StatefulMadrasah(madrasah: resource),
         );
       },
+    );
+  }
+}
+
+class StatefulMadrasah extends ConsumerStatefulWidget {
+  const StatefulMadrasah({
+    super.key,
+    required this.madrasah,
+  });
+
+  final dynamic madrasah;
+
+  @override
+  MadrasahState createState() => MadrasahState();
+}
+
+class MadrasahState extends ConsumerState<StatefulMadrasah> {
+  String? activeSection;
+  dynamic activeInfo;
+  final ScrollController sectionController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    activeSection = 'introduction';
+  }
+
+  updateSection(String value) {
+    setState(() {
+      activeSection = value;
+      activeInfo = null;
+    });
+  }
+
+  updateInfo(dynamic info) {
+    setState(() {
+      activeInfo = info;
+      activeSection = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return ItemContent(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 10, bottom: 15),
+          child: Text('Info', style: textTheme.labelLarge),
+        ),
+        Container(
+          margin: const EdgeInsets.only(bottom: 30),
+          constraints: BoxConstraints(
+            maxHeight: screenHeight * 0.4,
+          ),
+          child: Scrollbar(
+            thumbVisibility: true,
+            controller: sectionController,
+            child: SingleChildScrollView(
+              controller: sectionController,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  MadrasahSection(
+                    title: 'Introduction',
+                    isSelected: activeSection == 'introduction',
+                    onSelected: () => updateSection('introduction'),
+                  ),
+                  ...widget.madrasah.madrasahInfos.map((info) {
+                    return MadrasahSection(
+                      title: info.label,
+                      isSelected: activeInfo?.id == info.id,
+                      onSelected: () => updateInfo(info),
+                    );
+                  }),
+                  MadrasahSection(
+                    title: 'Gallery',
+                    isSelected: activeSection == 'gallery',
+                    onSelected: () => updateSection('gallery'),
+                  ),
+                  MadrasahSection(
+                    title: 'Document',
+                    isSelected: activeSection == 'document',
+                    onSelected: () => updateSection('document'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Text(
+            widget.madrasah.title,
+            style: textTheme.headlineMedium,
+          ),
+        ),
+        if (activeSection == 'introduction') ...[
+          Text('Introduction', style: textTheme.labelMedium),
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: HtmlText(
+              text: widget.madrasah.introduction,
+            ),
+          ),
+        ] else if (activeSection == 'gallery') ...[
+          Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            child: Text('Gallery', style: textTheme.labelMedium),
+          ),
+          ...widget.madrasah.madrasahPhotos.map((photo) {
+            return Container(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: ResponsiveImage(
+                image: photo.image,
+                model: 'madrasahPhoto',
+              ),
+            );
+          }),
+        ] else if (activeSection == 'document') ...[
+          const DescriptionItem(
+            title: 'Download:',
+            description: Align(
+              alignment: Alignment.topLeft,
+              child: Icon(
+                Icons.download,
+                size: 24,
+              ),
+            ),
+          ),
+        ] else if (activeInfo != null) ...[
+          Text(activeInfo.label, style: textTheme.labelMedium),
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: HtmlText(text: activeInfo.info),
+          ),
+        ],
+      ],
+    );
+  }
+}
+
+class MadrasahSection extends StatelessWidget {
+  const MadrasahSection({
+    super.key,
+    required this.title,
+    required this.isSelected,
+    required this.onSelected,
+  });
+
+  final String title;
+  final bool isSelected;
+  final void Function()? onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 15),
+      child: InkWell(
+        onTap: onSelected,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: ThemeColors.color7,
+          ),
+          padding: const EdgeInsets.all(15),
+          child: Text(
+            title,
+            style: textTheme.labelMedium?.copyWith(
+              color: isSelected ? ThemeColors.color3 : ThemeColors.color4,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/settings/image.dart
+++ lib/settings/image.dart
@@ -1,5 +1,8 @@
 const Map imageSettings = {
   'book': {
     'image': {'width': 240, 'height': 360}
-  }
+  },
+  'madrasahPhoto': {
+    'image': {'width': 600, 'height': 600}
+  },
 };
