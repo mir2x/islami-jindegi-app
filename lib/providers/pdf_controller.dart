1a2698ad2bff66e9e44d677882ead86b9532cd68
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-04-09 14:28:00 +0600

    Add pdf_controller provider

---
+++ lib/providers/pdf_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pdfx/pdfx.dart';
+import 'package:internet_file/internet_file.dart';
+import 'package:native_app/helpers/file_utils.dart';
+import 'local_file.dart';
+
+final pdfControllerProvider =
+    FutureProvider.autoDispose.family((ref, Map document) async {
+  var documentPath = document['id'];
+  var localFile = await ref.read(localFileProvider(documentPath).future);
+
+  dynamic pdfDoc;
+
+  if (localFile != null) {
+    pdfDoc = PdfDocument.openFile(localFile.path);
+  } else {
+    pdfDoc = PdfDocument.openData(
+      InternetFile.get(fileSrcUrl(document)),
+    );
+  }
+
+  var pdfController = PdfController(document: pdfDoc);
+
+  return pdfController;
+});
