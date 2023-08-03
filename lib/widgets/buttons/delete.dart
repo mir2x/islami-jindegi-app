2aa81ac013d73ef7d3bb03f97ca1a5b2f116d24a
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-08-03 18:40:22 +0600

    Show alert during file delete

--- lib/widgets/buttons/delete.dart
+++ lib/widgets/buttons/delete.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:native_app/providers/check_downloaded_file.dart';
 import 'package:native_app/providers/local_file.dart';
 import 'package:native_app/theme/colors.dart';
@@ -14,6 +15,9 @@ class DeleteButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var locales = AppLocalizations.of(context)!;
+    var textTheme = Theme.of(context).textTheme;
+
     return IconButton(
       icon: const Icon(
         Icons.delete,
@@ -22,16 +26,50 @@ class DeleteButton extends ConsumerWidget {
       iconSize: 30,
       color: Colors.red,
       onPressed: () async {
-        var localFile = await ref.read(
-          localFileProvider(filePath).future,
+        Widget cancelButton = TextButton(
+          child: Text(locales.no, style: textTheme.labelLarge),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         );
 
-        if (localFile != null) {
-          await localFile.delete();
-          await ref
-              .read(checkDownloadedFileProvider(filePath).notifier)
-              .check(filePath);
-        }
+        Widget continueButton = TextButton(
+          child: Text(
+            locales.yes,
+            style: textTheme.labelLarge?.copyWith(color: Colors.red),
+          ),
+          onPressed: () async {
+            Navigator.of(context).pop();
+
+            var localFile = await ref.read(
+              localFileProvider(filePath).future,
+            );
+
+            if (localFile != null) {
+              await localFile.delete();
+              await ref
+                  .read(checkDownloadedFileProvider(filePath).notifier)
+                  .check(filePath);
+            }
+          },
+        );
+
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(locales.fileDelete),
+              content: Text(
+                locales.doYouWantToDeleteFile,
+                style: textTheme.labelMedium,
+              ),
+              actions: [
+                cancelButton,
+                continueButton,
+              ],
+            );
+          },
+        );
       },
     );
   }
