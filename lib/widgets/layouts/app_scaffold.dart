372cc4fb6c26a09d725e7bbf4533f15cf3e9d8d7
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-08-10 13:37:17 +0600

    Override the back button of Android

--- lib/widgets/layouts/app_scaffold.dart
+++ lib/widgets/layouts/app_scaffold.dart
@@ -49,209 +49,220 @@ class AppScaffold extends ConsumerWidget {
       data: (preferences) {
         String theme = preferences.getString('theme') ?? 'dark';
 
-        return Scaffold(
-          key: sKey,
-          appBar: AppBar(
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: isHome
-                  ? theme == 'dark'
-                      ? SvgPicture.asset(
-                          'assets/images/logos/logo.svg',
-                          fit: BoxFit.scaleDown,
-                          width: 40,
-                          height: 35,
-                        )
-                      : SvgPicture.asset(
-                          'assets/images/logos/logo-light.svg',
-                          fit: BoxFit.scaleDown,
-                          width: 40,
-                          height: 35,
-                        )
-                  : IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () async {
-                        try {
-                          onBackPressed != null
-                              ? await onBackPressed!()
-                              : await QR.back();
-                        } catch (error) {
-                          await QR.navigator.replaceAll('/');
-                        }
-                      },
-                    ),
-            ),
-            title: title,
-            centerTitle: true,
-            actions: <Widget>[
-              const NotificationButton(),
-              Padding(
-                padding: const EdgeInsets.only(right: 5),
+        return BackButtonListener(
+          onBackButtonPressed: () async {
+            if (onBackPressed != null) {
+              await onBackPressed!();
+              return Future.value(true);
+            } else {
+              return Future.value(false);
+            }
+          },
+          child: Scaffold(
+            key: sKey,
+            appBar: AppBar(
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 15),
                 child: isHome
-                    ? PopupMenuButton<int>(
-                        child: const SizedBox(
-                          width: 40,
-                          child: Icon(
-                            Icons.more_vert,
-                          ),
-                        ),
-                        itemBuilder: (BuildContext context) =>
-                            <PopupMenuEntry<int>>[
-                          PopupMenuItem<int>(
-                            value: 0,
-                            child: Text(locales.settings),
-                          ),
-                          PopupMenuItem<int>(
-                            value: 1,
-                            child: Text(locales.contactUs),
-                          ),
-                          PopupMenuItem<int>(
-                            value: 2,
-                            child: Text(locales.shareThisApp),
-                          ),
-                          PopupMenuItem<int>(
-                            value: 3,
-                            child: Text(locales.rateThisApp),
-                          ),
-                          PopupMenuItem<int>(
-                            value: 4,
-                            child: Text(locales.iphoneAppLink),
-                          ),
-                          PopupMenuItem<int>(
-                            value: 5,
-                            child: Text(locales.websiteLink),
-                          ),
-                          PopupMenuItem<int>(
-                            value: 6,
-                            child: Text(locales.importantMatters),
-                          ),
-                        ],
-                        onSelected: (int item) {
-                          const androidAppLink =
-                              'https://play.google.com/store/apps/details?id=com.islamidars';
-
-                          const iOSAppLink =
-                              'https://apps.apple.com/app/islami-dars/id6447244518';
-
-                          switch (item) {
-                            case 0:
-                              QR.to('settings');
-                              break;
-                            case 1:
-                              QR.to('contact-us');
-                              break;
-                            case 2:
-                              if (Platform.isAndroid) {
-                                Share.share(androidAppLink);
-                              } else if (Platform.isIOS) {
-                                Share.share(iOSAppLink);
-                              }
-                              break;
-                            case 3:
-                              final Uri url = Uri.parse(androidAppLink);
-                              launchUrl(url);
-                              break;
-                            case 4:
-                              final Uri url = Uri.parse(iOSAppLink);
-                              launchUrl(url);
-                              break;
-                            case 5:
-                              final Uri url =
-                                  Uri.parse('https://islamidars.com');
-                              launchUrl(url);
-                              break;
-                            case 6:
-                              QR.to('important-matters');
-                              break;
+                    ? theme == 'dark'
+                        ? SvgPicture.asset(
+                            'assets/images/logos/logo.svg',
+                            fit: BoxFit.scaleDown,
+                            width: 40,
+                            height: 35,
+                          )
+                        : SvgPicture.asset(
+                            'assets/images/logos/logo-light.svg',
+                            fit: BoxFit.scaleDown,
+                            width: 40,
+                            height: 35,
+                          )
+                    : IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        onPressed: () async {
+                          try {
+                            onBackPressed != null
+                                ? await onBackPressed!()
+                                : await QR.back();
+                          } catch (error) {
+                            await QR.navigator.replaceAll('/');
                           }
                         },
-                      )
-                    : InkWell(
-                        onTap: () => sKey.currentState!.openEndDrawer(),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12),
-                          child: SvgPicture.asset(
-                            'assets/images/icons/menu.svg',
-                            fit: BoxFit.scaleDown,
-                            width: 30,
-                            height: 30,
+                      ),
+              ),
+              title: title,
+              centerTitle: true,
+              actions: <Widget>[
+                const NotificationButton(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: isHome
+                      ? PopupMenuButton<int>(
+                          child: const SizedBox(
+                            width: 40,
+                            child: Icon(
+                              Icons.more_vert,
+                            ),
+                          ),
+                          itemBuilder: (BuildContext context) =>
+                              <PopupMenuEntry<int>>[
+                            PopupMenuItem<int>(
+                              value: 0,
+                              child: Text(locales.settings),
+                            ),
+                            PopupMenuItem<int>(
+                              value: 1,
+                              child: Text(locales.contactUs),
+                            ),
+                            PopupMenuItem<int>(
+                              value: 2,
+                              child: Text(locales.shareThisApp),
+                            ),
+                            PopupMenuItem<int>(
+                              value: 3,
+                              child: Text(locales.rateThisApp),
+                            ),
+                            PopupMenuItem<int>(
+                              value: 4,
+                              child: Text(locales.iphoneAppLink),
+                            ),
+                            PopupMenuItem<int>(
+                              value: 5,
+                              child: Text(locales.websiteLink),
+                            ),
+                            PopupMenuItem<int>(
+                              value: 6,
+                              child: Text(locales.importantMatters),
+                            ),
+                          ],
+                          onSelected: (int item) {
+                            const androidAppLink =
+                                'https://play.google.com/store/apps/details?id=com.islamidars';
+
+                            const iOSAppLink =
+                                'https://apps.apple.com/app/islami-dars/id6447244518';
+
+                            switch (item) {
+                              case 0:
+                                QR.to('settings');
+                                break;
+                              case 1:
+                                QR.to('contact-us');
+                                break;
+                              case 2:
+                                if (Platform.isAndroid) {
+                                  Share.share(androidAppLink);
+                                } else if (Platform.isIOS) {
+                                  Share.share(iOSAppLink);
+                                }
+                                break;
+                              case 3:
+                                final Uri url = Uri.parse(androidAppLink);
+                                launchUrl(url);
+                                break;
+                              case 4:
+                                final Uri url = Uri.parse(iOSAppLink);
+                                launchUrl(url);
+                                break;
+                              case 5:
+                                final Uri url =
+                                    Uri.parse('https://islamidars.com');
+                                launchUrl(url);
+                                break;
+                              case 6:
+                                QR.to('important-matters');
+                                break;
+                            }
+                          },
+                        )
+                      : InkWell(
+                          onTap: () => sKey.currentState!.openEndDrawer(),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: SvgPicture.asset(
+                              'assets/images/icons/menu.svg',
+                              fit: BoxFit.scaleDown,
+                              width: 30,
+                              height: 30,
+                            ),
                           ),
                         ),
-                      ),
-              ),
-            ],
-          ),
-          drawer: drawer,
-          endDrawer: Drawer(
-            child: ListView(
-              padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                    QR.navigator.replaceAll('/');
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: Text(
-                      locales.home,
-                      style: textTheme.titleLarge?.copyWith(fontSize: 24),
-                      textAlign: TextAlign.right,
-                    ),
-                  ),
                 ),
-                DrawerLink(title: locales.quran, route: 'quran'),
-                DrawerLink(title: locales.books, route: 'books'),
-                DrawerLink(title: locales.bayans, route: 'bayans'),
-                DrawerLink(title: locales.malfuzat, route: 'malfuzat'),
-                DrawerLink(title: locales.masail, route: 'masail'),
-                DrawerLink(title: locales.duaDurud, route: 'duas'),
-                DrawerLink(title: locales.articles, route: 'articles'),
-                DrawerLink(title: locales.news, route: 'news'),
-                DrawerLink(title: locales.madrasah, route: 'madrasahs'),
-                DrawerLink(title: locales.namazTime, route: 'namaz-times'),
-                DrawerLink(title: locales.qiblah, route: 'qiblah'),
-                DrawerLink(title: locales.donation, route: 'donation'),
-                DrawerLink(title: locales.settings, route: 'settings'),
-                DrawerLink(title: locales.bookmarks, route: 'bookmarks'),
-                DrawerLink(title: locales.aboutUs, route: 'about'),
-                DrawerLink(title: locales.contactUs, route: 'contact-us'),
               ],
             ),
-          ),
-          body: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: theme == 'dark'
-                    ? const AssetImage(
-                        'assets/images/icons/background-pattern-dark.png',
-                      )
-                    : const AssetImage(
-                        'assets/images/icons/background-pattern-light.png',
+            drawer: drawer,
+            endDrawer: Drawer(
+              child: ListView(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                      QR.navigator.replaceAll('/');
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Text(
+                        locales.home,
+                        style: textTheme.titleLarge?.copyWith(fontSize: 24),
+                        textAlign: TextAlign.right,
                       ),
-                repeat: ImageRepeat.repeat,
+                    ),
+                  ),
+                  DrawerLink(title: locales.quran, route: 'quran'),
+                  DrawerLink(title: locales.books, route: 'books'),
+                  DrawerLink(title: locales.bayans, route: 'bayans'),
+                  DrawerLink(title: locales.malfuzat, route: 'malfuzat'),
+                  DrawerLink(title: locales.masail, route: 'masail'),
+                  DrawerLink(title: locales.duaDurud, route: 'duas'),
+                  DrawerLink(title: locales.articles, route: 'articles'),
+                  DrawerLink(title: locales.news, route: 'news'),
+                  DrawerLink(title: locales.madrasah, route: 'madrasahs'),
+                  DrawerLink(title: locales.namazTime, route: 'namaz-times'),
+                  DrawerLink(title: locales.qiblah, route: 'qiblah'),
+                  DrawerLink(title: locales.donation, route: 'donation'),
+                  DrawerLink(title: locales.settings, route: 'settings'),
+                  DrawerLink(title: locales.bookmarks, route: 'bookmarks'),
+                  DrawerLink(title: locales.aboutUs, route: 'about'),
+                  DrawerLink(title: locales.contactUs, route: 'contact-us'),
+                ],
               ),
             ),
-            constraints: const BoxConstraints.expand(),
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                if (constraints.maxWidth < 768) {
-                  return body;
-                } else {
-                  double screenWidth = MediaQuery.of(context).size.width;
+            body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: theme == 'dark'
+                      ? const AssetImage(
+                          'assets/images/icons/background-pattern-dark.png',
+                        )
+                      : const AssetImage(
+                          'assets/images/icons/background-pattern-light.png',
+                        ),
+                  repeat: ImageRepeat.repeat,
+                ),
+              ),
+              constraints: const BoxConstraints.expand(),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  if (constraints.maxWidth < 768) {
+                    return body;
+                  } else {
+                    double screenWidth = MediaQuery.of(context).size.width;
 
-                  return Container(
-                    padding: EdgeInsets.only(
-                      left: screenWidth * 0.06,
-                      right: screenWidth * 0.06,
-                    ),
-                    child: body,
-                  );
-                }
-              },
+                    return Container(
+                      padding: EdgeInsets.only(
+                        left: screenWidth * 0.06,
+                        right: screenWidth * 0.06,
+                      ),
+                      child: body,
+                    );
+                  }
+                },
+              ),
             ),
+            bottomNavigationBar: bottomBar,
           ),
-          bottomNavigationBar: bottomBar,
         );
       },
     );
