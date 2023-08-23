19a64980206dca963c013c49eca5f293f523d23e
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-08-23 17:50:44 +0600

    Add fallback hijri date

--- lib/widgets/calendar/hijri_date.dart
+++ lib/widgets/calendar/hijri_date.dart
@@ -3,6 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hijri/hijri_calendar.dart';
 import 'package:native_app/providers/hijri_date_settings.dart';
+import 'package:native_app/providers/preferences.dart';
 import 'package:native_app/helpers/adjusted_hijri_date.dart';
 import 'package:native_app/helpers/split_hijri_date.dart';
 
@@ -23,14 +24,43 @@ class HijriDate extends ConsumerWidget {
     var settingsProvider = ref.watch(hijriDateSettingsProvider);
 
     return settingsProvider.when(
-      loading: () => Container(
-        padding: const EdgeInsets.only(bottom: 8),
-        child: const SizedBox(
-          width: 15,
-          height: 15,
-          child: CircularProgressIndicator(strokeWidth: 2),
-        ),
-      ),
+      loading: () {
+        var prefs = ref.watch(preferencesProvider);
+
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            prefs.when(
+              loading: () => const SizedBox(
+                width: 15,
+                height: 15,
+              ),
+              error: (error, _) => Text(error.toString()),
+              data: (preferences) {
+                String? hijriDate = preferences.getString('hijriDate');
+
+                if (hijriDate != null) {
+                  return Text(hijriDate);
+                } else {
+                  HijriCalendar date = HijriCalendar.now();
+
+                  Map h = splitHijriDate(date, locales, currentLang);
+                  String hijriDate =
+                      '${h['day']} ${h['month']}, ${h['year']} ${locales.hijri}';
+
+                  return Text(hijriDate);
+                }
+              },
+            ),
+            const SizedBox(width: 10),
+            const SizedBox(
+              width: 15,
+              height: 15,
+              child: CircularProgressIndicator(strokeWidth: 2),
+            ),
+          ],
+        );
+      },
       error: (error, _) => Text(error.toString()),
       data: (Map settings) {
         HijriCalendar today;
