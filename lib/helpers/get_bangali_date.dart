293a7e876f07b25ee587f7ece313086074afb907
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-10-26 16:40:06 +0600

    Add season to bangali date

--- lib/helpers/get_bangali_date.dart
+++ lib/helpers/get_bangali_date.dart
@@ -2,5 +2,5 @@ import 'package:native_app/objects/bongabdo.dart';
 
 String getBangaliDate() {
   final today = Bongabdo.now();
-  return '${today.bDay} ${today.bMonth}, ${today.bYear} বঙ্গাব্দ';
+  return '${today.bDay} ${today.bMonth}, ${today.bYear} ${today.bSeason}';
 }
