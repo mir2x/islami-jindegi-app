21b9594cbde35e14c64d971004505462d5e8c1f0
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-11-20 16:32:11 +0600

    Use new method for Bondabdo

--- lib/helpers/get_bangali_date.dart
+++ lib/helpers/get_bangali_date.dart
@@ -1,6 +1,6 @@
 import 'package:native_app/objects/bongabdo.dart';
 
 String getBangaliDate() {
-  final today = Bongabdo.now();
+  final today = Bongabdo.now('new');
   return '${today.bDay} ${today.bMonth}, ${today.bYear} ${today.bSeason}কাল';
 }
