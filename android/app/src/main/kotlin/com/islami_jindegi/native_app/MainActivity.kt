38aa06d7c83b939637c7c282f9ee1ffab6b5bd90
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2024-01-24 22:26:46 +0600

    Extend MainActivity from AudioServiceActivity

--- android/app/src/main/kotlin/com/islami_jindegi/native_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/islami_jindegi/native_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.islami_jindegi.native_app
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: AudioServiceActivity() {
 }
