7013d747f4e928107611220bf0f65a5f72434dbf
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2024-02-19 18:03:12 +0600

    Update local database to version 6

--- lib/local_server/resource/local_resource_api.dart
+++ lib/local_server/resource/local_resource_api.dart
@@ -53,7 +53,7 @@ class LocalResourceAPI extends _$LocalResourceAPI {
   LocalResourceAPI()
       : super(
           LazyDatabase(() async {
-            int dataVersion = 5;
+            int dataVersion = 6;
 
             final dbFolder = await getApplicationDocumentsDirectory();
             final file = File(
