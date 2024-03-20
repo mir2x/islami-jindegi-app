8ed075f0bfb12b715ed856ef1902e0337c3fb34b
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2024-03-20 23:27:55 +0600

    Update local data

--- lib/local_server/resource/local_resource_api.dart
+++ lib/local_server/resource/local_resource_api.dart
@@ -53,7 +53,7 @@ class LocalResourceAPI extends _$LocalResourceAPI {
   LocalResourceAPI()
       : super(
           LazyDatabase(() async {
-            int dataVersion = 7;
+            int dataVersion = 8;
 
             final dbFolder = await getApplicationDocumentsDirectory();
             final file = File(
