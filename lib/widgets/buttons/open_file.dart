ad9e15925c6d50756b8b7160bd9cfe3a48c9a1dd
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2025-06-19 19:10:19 +0600

    Add explorer code

--- lib/widgets/buttons/open_file.dart
+++ lib/widgets/buttons/open_file.dart
@@ -32,6 +32,15 @@ class OpenFile extends StatelessWidget {
                 : await getApplicationSupportDirectory();
 
             if (downloadDir != null && path != null) {
+              // String fullPath = p.join(downloadDir.path, path);
+              // List list = fullPath.split('/');
+              // String directoryPath = list.sublist(0, list.length - 1).join('/');
+              // Process.run(
+              //   'explorer',
+              //   [directoryPath],
+              //   workingDirectory: directoryPath,
+              // );
+
               await OpenFilex.open(
                 p.join(downloadDir.path, path),
               );
